5cd439865ad31f97ee9dae29c0e3aa4dbc30d29b
Adin Gates <132237907+Adin-Gates@users.noreply.github.com>
2026-07-29 15:24:03 -0600

    Added path resolution, enviroment show root, template json and template resolver in paths.py

---
+++ python/juno/paths.py
@@ -0,0 +1,34 @@
+import os
+from juno.config import load_config
+from pathlib import Path
+
+
+
+# get_show_root : extracts the show root form the environemt
+def get_show_root():
+    show_root = os.environ.get("JUNO_SHOW_ROOT")
+
+    if show_root is None:
+        raise EnvironmentError("No show root found.")
+
+
+    return Path(show_root)
+
+
+# resolve_template : given a template name string and tokens that naming scheme needs this will generate the corrent path
+def resolve_template(template_name, **tokens):
+
+    template_path = Path(__file__).parent.parent.parent / "config" / "templates.json"
+
+    template_dict = load_config(template_path)
+
+    template = template_dict[template_name]
+
+    result = template.format(**tokens)
+
+    show_root = get_show_root()
+
+    return show_root / result
+
+x = resolve_template("shot_work_dir", show_code="DEMO", sequence="SQ010", shot="SH0010", department="fx")
+print(x)
